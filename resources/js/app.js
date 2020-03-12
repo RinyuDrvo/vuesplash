e0f53cffd9ee7f26e98dda37980fb43fa0cd6bd6
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-03-12 05:42:46 +0000

    make vuex store

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
-//ルーティングの定義
 import router from './router'
-//ルートコンポーネント
+import store from './store'
 import App from './App.vue'
 
 new Vue({
   el: '#app',
-  router, //ルーティングの定義読み込み
-  components: { App }, //ルートコンポーネントの使用を宣言する
-  template: '<app />' //ルートコンポーネントを描画する
+  router,
+  store,
+  components: { App },
+  template: '<App />'
 })
