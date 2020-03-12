6da9dc2f25b60773a417063a02bf3d9fabfd9a89
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-03-12 17:25:28 +0000

    fix auth store

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -1,9 +1,10 @@
 import axios from "axios"
-import { OK } from '../util'
+import { OK, UNPROCESSABLE_ENTITY } from '../util'
 
 const state = {
   user: null,
-  apiStatus: null
+  apiStatus: null,
+  loginErrorMessages: null
 }
 
 const getters = {
@@ -17,6 +18,9 @@ const mutations = {
   },
   setApiStatus(state, status) {
     state.apiStatus = status
+  },
+  setLoginErrorMessages(state, messages) {
+    state.loginErrorMessages = messages
   }
 }
 
@@ -37,7 +41,11 @@ const actions = {
     }
 
     context.commit('setApiStatus', false)
-    context.commit('error/setCode', response.status, { root: true })
+    if (response.status === UNPROCESSABLE_ENTITY) {
+      context.commit('setLoginErrorMessages', response.data.errors)
+    } else {
+      context.commit('error/setCode', response.status, { root: true })
+    }
   },
   async logout(context) {
     const response = await axios.post('/api/logout')
