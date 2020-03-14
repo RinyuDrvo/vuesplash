10d1677c4fb77485c958953c51540c68a0538d8b
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-03-14 14:44:00 +0000

    make submit photo form

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -3,7 +3,7 @@
     <RouterLink class="navbar__brand" to="/">Vuesplash</RouterLink>
     <div class="navbar__menu">
       <div class="navbar__item" v-if="isLogin">
-        <button class="button">
+        <button class="button" @click="showForm = ! showForm">
           <i class="icon ion-md-add"></i>
           Submit a photo
         </button>
@@ -13,11 +13,22 @@
         <RouterLink class="button button--link" to="/login">Login / Register</RouterLink>
       </div>
     </div>
+    <PhotoForm v-model="showForm" />
   </nav>
 </template>
 
 <script>
+import PhotoForm from "./PhotoForm.vue";
+
 export default {
+  components: {
+    PhotoForm
+  },
+  data () {
+    return {
+      showForm: false
+    }
+  },
   computed: {
     isLogin() {
       return this.$store.getters["auth/check"];
